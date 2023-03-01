44a7e8da2f71a673ddcef2ad91d201c305b2e1f9
violetakokorevi <91872388+violetakokorevicha@users.noreply.github.com>
2023-03-01 15:06:15 +0000

    wqdrfrweftrwe

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
-def atrod_virsrakstu(url):
+
+def atrodi_virsrakstu(url):
   r = requests.get(url)
   html = r.text
   soup = BeautifulSoup(html, "html.parser")
   virsraksti = []
   for h1 in soup.find_all("h1"):
-    virsraksti = h1.text
-    virsraksti.append(virsraksti)
-    return virsraksti
+    virsraksts = h1.text
+    virsraksti.append(virsraksts)
+  return virsraksti
+
 url = input("Ievadi URl:")
-virsraksti = atrod_virsrakstu(url)
+virsraksti = atrodi_virsrakstu(url)
 print(virsraksti)
   
